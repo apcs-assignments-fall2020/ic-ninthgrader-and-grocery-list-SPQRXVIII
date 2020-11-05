c1d953f8ec2953e6e35eb5a12673ce140ca60c03
SPQRXVIII <Aftersilver001@gmail.com>
2020-11-05 17:29:59 +0000

    finished

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,31 @@
+import java.util.*; 
 public class GroceryList {
-    
+    GroceryList(){
+        v1 = new Vector<String>();
+    }
+
+    public void add(String item){
+        v1.add(item);
+    }
+
+    public void remove(String item){
+        for(String str: v1){
+            if(str.equals(item)){
+                int i = v1.indexOf(item);
+                v1.remove(i);
+            }
+        }
+    }
+
+    @Override
+    public String toString(){
+        String str = "Grocery List ";
+        for(String item: v1){
+            str = str + item + " ";
+        }
+       return str;
+    }
+
+    private Vector<String> v1;
 }
+
